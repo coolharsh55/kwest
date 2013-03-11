ba4379c1e2e7c1d0d5aecb99142450515110c932
coolharsh55 <coolharsh55@gmail.com>
2013-03-11 15:14:15 +0530

    extract on load

--- include/plugin_extract.h
+++ include/plugin_extract.h
@@ -1,6 +1,6 @@
 #ifndef PLUGIN_EXTRACT_H
 #define PLUGIN_EXTRACT_H
 
-struct plugin_extraction_entry *load_extractor_plugin(void);
+struct plugin_extraction_entry *load_extract_plugin(void);
 
 #endif
--- src/plugin_extract.c
+++ src/plugin_extract.c
@@ -193,7 +193,7 @@ static int on_unload(struct plugin_extraction_entry *plugin)
 	return KW_SUCCESS;
 }
 
-struct plugin_extraction_entry *load_extractor_plugin()
+struct plugin_extraction_entry *load_extract_plugin()
 {
 	static struct plugin_extraction_entry *plugin = NULL;
 	if (plugin == NULL) {
